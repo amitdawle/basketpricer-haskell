50815675024fea7c566f8c67e24625326a399604
amit <amit.dawle@gmail.com>
2016-05-02 22:20:02 +0100

    Added new pricer. Moved tests

--- basketpricer/src/BasketPricer.hs
+++ basketpricer/src/BasketPricer.hs
@@ -1,17 +1,45 @@
 module BasketPricer ( Item(Apple, Banana),
                       simplePrice,
-                      buyOneGetOneFree ) where
+                      buyOneGetOneFree,
+                      buyTwoGetOneFree,
+                      buyOneGetOneHalfPrice
+                      ) where
+
+import Data.List
+
 
 data Item = Banana | Apple deriving (Show, Eq)
 type Basket = [Item]
 type Discount = (Double, [Item])
+type Promotion = Basket -> Discount
+type Promotions = [Promotion]
 
 price Apple  = 0.6
 price Banana = 0.3
 
-simplePrice :: Basket -> Double
-simplePrice = (foldl (+) 0.0 ).map (price)
+simplePrice :: Basket -> Promotions -> Double
+simplePrice basket promotions = basketPrice - totalDiscount
+                where totalDiscount = sum.map (fst.fst) $ [ applyPromotion p basket | p <- promotions   ]
+                      basketPrice = (foldl (+) 0.0 ).map (price) $ basket
 
 buyOneGetOneFree :: Item -> Basket -> Discount
 buyOneGetOneFree item basket = if( length eligibleItems >= 2 ) then (price item , [item, item]) else (0.0, [])
                       where eligibleItems = filter ( == item) basket
+
+buyOneGetOneHalfPrice :: Item -> Basket -> Discount
+buyOneGetOneHalfPrice item basket = if( length eligibleItems >= 2 ) then ((price item) * 0.5, [item, item]) else (0.0, [])
+                      where eligibleItems = filter ( == item) basket
+
+buyTwoGetOneFree :: Item -> Basket -> Discount
+buyTwoGetOneFree item basket = if( length eligibleItems >= 3 ) then ((price item) , [item, item, item]) else (0.0, [])
+                      where eligibleItems = filter ( == item) basket
+
+applyPromotion :: Promotion -> Basket -> (Discount, Basket)
+applyPromotion p [] = ((0.0, []), [] )
+applyPromotion p basket = if( discount == 0.0) then ((0.0, []), newBasket)
+                          else let
+                              ((moreDiscount, moreDicountedItems),  updatedBasket) = applyPromotion p newBasket
+                              totalDiscount = ( discount + moreDiscount , discountedItems ++ moreDicountedItems  )
+                              in (totalDiscount, updatedBasket)
+             where (discount, discountedItems) = p basket
+                   newBasket = basket \\ discountedItems
--- basketpricer/src/BasketPricerTest.hs
+++ basketpricer/src/BasketPricerTest.hs
@@ -1,15 +1,60 @@
 import Test.Hspec
 import Test.QuickCheck
-import BasketPricer ( Item(Apple, Banana), simplePrice )
+import BasketPricer ( Item(Apple, Banana),
+                      simplePrice,
+                      buyOneGetOneFree,
+                      buyTwoGetOneFree,
+                      buyOneGetOneHalfPrice,
+                     )
 
-main = hspec $ describe "simplePriceBasket" $ do
+main = hspec $
+ describe "simplePriceBasket" $ do
   it "works for empty basket" $ do
-    simplePrice [] `shouldBe` 0.0
+    simplePrice [] [] `shouldBe` 0.0
 
-  it "works for baskets with some items" $ do
-    simplePrice [Apple] `shouldBe` 0.6
-    simplePrice [Apple, Apple] `shouldBe` 1.2
-    simplePrice [Banana] `shouldBe` 0.3
-    simplePrice [Banana, Banana] `shouldBe` 0.6
-    simplePrice [Apple, Apple, Apple] `shouldSatisfy` (\x -> abs( x - 1.8 ) <= 0.001)
+  it "works for baskets with some items, no promotions" $ do
+    simplePrice [Apple] [] `shouldBe` 0.6
+    simplePrice [Apple, Apple] [] `shouldBe` 1.2
+    simplePrice [Banana] [] `shouldBe` 0.3
+    simplePrice [Banana, Banana] [] `shouldBe` 0.6
+    simplePrice [Apple, Apple, Apple] [] `shouldSatisfy` (\x -> abs( x - 1.8 ) <= 0.001)
 
+  --- BOGOF
+  describe "buyOneGetOneFree" $ do
+      it "works for empty basket" $ do
+        buyOneGetOneFree Apple [] `shouldBe` (0.0, [])
+
+      it "works for baskets with two eligible items" $ do
+        buyOneGetOneFree Apple [Apple, Apple] `shouldBe` (0.6, [Apple, Apple])
+
+      it "works for baskets with two eligible items and other non eligible items" $ do
+        buyOneGetOneFree Apple [Apple, Apple, Apple, Banana] `shouldBe` (0.6, [Apple, Apple])
+
+      it "discount is applied once per invocation" $ do
+        buyOneGetOneFree Apple [Apple, Apple, Apple, Apple] `shouldBe` (0.6, [Apple, Apple])
+
+  describe "buyTwoGetOneFree" $ do
+      it "works for empty basket" $ do
+        buyTwoGetOneFree Apple [] `shouldBe` (0.0, [])
+
+      it "works for baskets with  eligible items" $ do
+        buyTwoGetOneFree Apple [Apple, Apple, Apple] `shouldBe` (0.6, [Apple, Apple, Apple])
+
+      it "works for baskets with three eilgible items and other non eligible items" $ do
+        buyTwoGetOneFree Apple [Apple, Apple, Apple, Banana] `shouldBe` (0.6, [Apple, Apple, Apple])
+
+      it "discount is applied once per invocation" $ do
+        buyTwoGetOneFree Apple [Apple, Apple, Apple, Apple, Apple, Apple] `shouldBe` (0.6, [Apple, Apple, Apple])
+
+  describe "buyOneGetOneHalfPrice" $ do
+      it "works for empty basket" $ do
+        buyOneGetOneHalfPrice Apple [] `shouldBe` (0.0, [])
+
+      it "works for baskets with  eligible items" $ do
+        buyOneGetOneHalfPrice Apple [Apple, Apple, Apple] `shouldBe` (0.3, [Apple, Apple])
+
+      it "works for baskets with two eilgible items and other non eligible items" $ do
+        buyOneGetOneHalfPrice Apple [Apple, Apple, Banana] `shouldBe` (0.3, [Apple, Apple])
+
+      it "discount is applied once per invocation" $ do
+        buyOneGetOneHalfPrice Apple [Apple, Apple, Apple, Apple, Apple, Apple] `shouldBe` (0.3, [Apple, Apple])
